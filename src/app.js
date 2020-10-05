82a4a3363f7035df6dbac1ceb1138926a2f2bc5b
PacoCoca <pacococacruz@gmail.com>
2020-10-05 11:44:46 +0200

    add body parser for json request

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import config from './config/index.js';
 import api from './api/index.js';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+// Middleware to parse the body of the requests
+app.use(bodyParser.json());
 
 app.use(api);
 
